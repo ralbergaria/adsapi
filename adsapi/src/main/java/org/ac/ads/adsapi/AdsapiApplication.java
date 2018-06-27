22f85ad4fcc360619549c1311e5ac12827376c06
rcarmo <rcarmo@192.168.1.58>
2018-06-27 11:18:11 -0300

    setting environment.

---
+++ adsapi/src/main/java/org/ac/ads/adsapi/AdsapiApplication.java
@@ -0,0 +1,12 @@
+package org.ac.ads.adsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdsapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdsapiApplication.class, args);
+	}
+}
---
+++ adsapi/src/main/resources/script.sql
@@ -0,0 +1,8 @@
+CREATE DATABASE ads_eservices;
+CREATE TABLE USER_ESERVICE (
+    LOGIN varchar,
+    PASSWORD varchar,
+    audit_information varchar,
+    eservices_preferences VARCHAR,
+    CUSTOMER_MASTER_ID VARCHAR
+);
